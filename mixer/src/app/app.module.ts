61e7453ec516b321030dc217ef2cd863591c213f
Kirk Peterson <peterson.kirk@gmail.com>
2024-05-06 18:19:18 -0600

    adds a second example component

--- mixer/src/app/app.module.ts
+++ mixer/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { appRoutes } from './app.routes';
 import { GroundComponent } from './play/ground.component';
+import { PickerComponent } from './play/picker.component';
 
 @NgModule({
-  declarations: [AppComponent, GroundComponent],
+  declarations: [AppComponent, GroundComponent, PickerComponent],
   imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
   providers: [],
   bootstrap: [AppComponent],
---
+++ mixer/src/app/play/picker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-picker',
+  templateUrl: './picker.component.html',
+  styleUrl: './picker.component.css',
+})
+export class PickerComponent {
+
+
+
+}
